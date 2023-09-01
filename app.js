ba58094626af809609a947f8cb571e291f0b5b8b
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-09-01 08:27:26 +0000

    [finish]js5

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-$(document).ready(function () {
-  // jQueryプログラムの内容
-  $('body').html('<h1>Hello jQuery!!</h1>');
-  // $( セレクタ ).メソッド ( パラメータ );
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
-
